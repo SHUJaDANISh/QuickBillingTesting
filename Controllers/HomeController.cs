c6edd9f85a20121fcf33501aaea188528efd105d
Shuja Danish <Shuja.Danish@primussoft.com>
2025-08-16 02:08:33 +0530

    add new controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickBillingTesting.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "API is running";
+    }
+
+}
